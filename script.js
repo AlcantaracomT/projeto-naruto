599bbbf429fed6e0635416792968bd6e40c893cf
Thiago Alcantara <alcantara.ta2213@gmail.com>
2022-09-26 18:42:44 -0300

    added js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener('play', () => {
+    document.getElementById("music").play();
+});
+
+// animação
+const carta = document.querySelector(".imgSelamento");
+const chain = document.getElementById("chain");
+const chain1 = document.getElementById("chain1");
+const chain2 = document.getElementById("chain2");
+const chain3 = document.getElementById("chain3");
+const chain4 = document.getElementById("chain4");
+const chain5 = document.getElementById("chain5");
+const chain6 = document.getElementById("chain6");
+const chain7 = document.getElementById("chain7");
+const chain8 = document.getElementById("chain8");
+const chain9 = document.getElementById("chain9");
+const chain10 = document.getElementById("chain10");
+const chain11 = document.getElementById("chain11");
+const chain12 = document.getElementById("chain12");
+const chain13 = document.getElementById("chain13");
+
+carta.addEventListener("click", () => {
+        chain.classList.add("chain");
+        chain1.classList.add("chain1");
+        chain2.classList.add("chain2");
+        chain3.classList.add("chain3");
+        chain4.classList.add("chain4");
+        chain5.classList.add("chain5");
+        chain6.classList.add("chain6");
+        chain7.classList.add("chain7");
+        chain8.classList.add("chain8");
+        chain9.classList.add("chain9");
+        chain10.classList.add("chain10");
+        chain11.classList.add("chain11");
+        chain12.classList.add("chain12");
+        chain13.classList.add("chain13");
+});
+// -------------------------------------------------
